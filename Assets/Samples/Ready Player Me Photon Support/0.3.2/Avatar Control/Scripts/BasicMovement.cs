fc1af26b5569d185dd9f2b06db151919108d13cb
Roni Keinan <ronikeinan17@gmail.com>
2025-03-25 21:05:14 +0200

    add jump

--- Assets/Samples/Ready Player Me Photon Support/0.3.2/Avatar Control/Scripts/BasicMovement.cs
+++ Assets/Samples/Ready Player Me Photon Support/0.3.2/Avatar Control/Scripts/BasicMovement.cs
@@ -7,31 +7,58 @@ namespace ReadyPlayerMe.PhotonSupport
     public class BasicMovement : MonoBehaviour
     {
         [SerializeField] private new GameObject camera;
-        
+
         private Animator animator;
         private PhotonView photonView;
-        
+        private Rigidbody rigidbody; // Ensure there's a Rigidbody component attached
+
         private readonly static int WALK_ANIM = Animator.StringToHash("Walking");
+        private bool isGrounded = true; // To check if the player is on the ground
 
         private void Awake()
         {
             animator = GetComponent<Animator>();
             photonView = GetComponent<PhotonView>();
-            
+            rigidbody = GetComponent<Rigidbody>(); // Get the Rigidbody component
+
             if (photonView.IsMine) camera.SetActive(true);
         }
-        
+
         private void Update()
         {
             if (photonView.IsMine)
             {
-                var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-                var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-            
-                transform.Rotate(0, x, 0);
-                transform.Translate(0, 0, z);
+                HandleMovement();
+                HandleJump();
+            }
+        }
 
-                animator.SetBool(WALK_ANIM, z != 0);
+        private void HandleMovement()
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+
+            animator.SetBool(WALK_ANIM, z != 0);
+        }
+
+        private void HandleJump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                rigidbody.AddForce(new Vector3(0, 5.0f, 0), ForceMode.Impulse);
+                isGrounded = false; // Reset in OnCollisionEnter
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            // Check if we collide with the ground
+            if (collision.contacts[0].normal.y > 0.5) // Ensures it's mostly vertical
+            {
+                isGrounded = true;
             }
         }
     }
