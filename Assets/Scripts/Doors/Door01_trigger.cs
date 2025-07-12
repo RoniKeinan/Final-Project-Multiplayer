b2326cfe87bfa2789ccc94f6e488e225a02b9389
Itai Galipoliti <itai.galipo@gmail.com>
2025-07-12 10:46:27 +0300

    riddle 3

--- Assets/Scripts/Doors/Door01_trigger.cs
+++ Assets/Scripts/Doors/Door01_trigger.cs
@@ -4,14 +4,20 @@ public class Door01_trigger : MonoBehaviour
 {
     public DoorController01_pr doorController;
 
+    private bool hasTriggered = false; 
+
     private void OnTriggerEnter(Collider other)
     {
+        if (hasTriggered)
+            return; 
         Debug.Log($"OnTriggerEnter called with object: {other.name}");
 
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player entered the trigger zone — attempting to close door.");
             doorController.CloseDoor();
+            hasTriggered = true;
+
         }
         else
         {
