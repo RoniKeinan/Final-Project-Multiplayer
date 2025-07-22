5a3404af44edfe4f59c5da75966d817cf0d5ab85
Itai Galipoliti <itai.galipo@gmail.com>
2025-07-22 13:43:18 +0300

    lead board fix

--- Assets/Scripts/Riddles/KeyHolder.cs
+++ Assets/Scripts/Riddles/KeyHolder.cs
@@ -3,7 +3,7 @@
 public class KeyHolder : MonoBehaviour
 {
     // Whether this player currently holds the key
-    public bool HasKey { get; private set; } = false;
+    public bool HasKey { get; private set; } = true;
 
     // Call this method to give the player the key
     public void GiveKey()
