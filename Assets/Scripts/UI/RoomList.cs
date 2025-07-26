6117f285ea703c5027e2360d410e7c31b15f8ef4
Roni Keinan <ronikeinan17@gmail.com>
2025-07-26 19:24:58 +0300

    Reapply "bugs fixing after test"
    This reverts commit aaf9cf5febc3c954ddf6f65a531873c62d8cec6c.

--- Assets/Scripts/UI/RoomList.cs
+++ Assets/Scripts/UI/RoomList.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
@@ -86,6 +87,7 @@ public class RoomList : MonoBehaviourPunCallbacks
         List<string> options = new List<string> { "1", "2", "3", "4", "5" };
         maxPlayersDropdown.AddOptions(options);
         maxPlayersDropdown.value = 0; // default is 1 player
+        charPanel.gameObject.SetActive(false);
 
 
         PhotonNetwork.ConnectUsingSettings();
